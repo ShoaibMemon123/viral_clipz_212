60debf13c96d913c80d248780a6157f2958f3405
ShoaibMemon123 <96578536+ShoaibMemon123@users.noreply.github.com>
2024-08-07 02:39:27 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,2 @@
-// Get the first 20 posts from WordPress, ordered by the date
-export async function getAllPostsFromWordPress(preview) {
-  const data = await fetchAPI(`
-    query AllPosts {
-      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
-        edges {
-          node {
-            title
-            excerpt
-            slug
-            date
-          }
-        }
-      }
-    }
-  `)
-
-  return data.posts
-}
+export async function getStaticProps({ preview = false }) {
+  const allPosts = await getAllPostsForHome(preview)
