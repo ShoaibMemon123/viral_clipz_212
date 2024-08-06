9a18b50322aab84543aa88cb5e6b73487b8d15a0
ShoaibMemon123 <96578536+ShoaibMemon123@users.noreply.github.com>
2024-08-07 01:57:56 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
+// Get the first 20 posts from WordPress, ordered by the date
+export async function getAllPostsFromWordPress(preview) {
+  const data = await fetchAPI(`
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+          }
+        }
+      }
+    }
+  `)
 
-  return {
-    props: { allPosts, preview },
-    revalidate: 10,
-  }
+  return data.posts
 }
