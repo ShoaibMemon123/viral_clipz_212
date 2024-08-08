85bdb25cdf91b01be2fd49d053e395c295439a1f
ShoaibMemon123 <96578536+ShoaibMemon123@users.noreply.github.com>
2024-08-08 17:19:10 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/zali56',
-        destination: 'https://vercel.com/guides/wordpress-with-vercel',
-        permanent: true,
-      }
-      }
-}
