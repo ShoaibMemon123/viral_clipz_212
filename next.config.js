ddf2616e2a92331018356a759c15407117e3e0b1
ShoaibMemon123 <96578536+ShoaibMemon123@users.noreply.github.com>
2024-08-08 18:44:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,3 +5,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://hdmediaa.com/sfgdfgs/', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
+}
